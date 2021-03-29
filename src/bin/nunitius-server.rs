dcdcbeab0a0c605dc9c8182593588483849a4618
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-03-29 15:00:53 +1100

    Add basic client and server code

--- src/bin/nunitius-server.rs
+++ src/bin/nunitius-server.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use std::io::Read;
+use std::net::TcpListener;
+
+fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:9999")?;
+
+    for stream in listener.incoming() {
+        let mut stream = stream?;
+
+        let mut text_from_connection = Vec::new();
+        stream.read_to_end(&mut text_from_connection)?;
+
+        let text_from_connection = String::from_utf8_lossy(&text_from_connection);
+        println!("{}", text_from_connection);
+    }
+
+    Ok(())
+}

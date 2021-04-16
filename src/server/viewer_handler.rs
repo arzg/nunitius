ec4e4298db2f81f61791f80f05bddefd70180215
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-04-16 12:10:44 +1000

    Use opaque viewer IDs instead of i32 primitive

--- src/server/viewer_handler.rs
+++ src/server/viewer_handler.rs
@@ -3,18 +3,33 @@ use flume::{Receiver, Selector};
 use log::{error, info};
 use std::cell::RefCell;
 use std::collections::HashMap;
-use std::io;
 use std::net::TcpStream;
+use std::{io, mem};
+
+#[derive(Default)]
+struct ViewerIdGenerator {
+    current: ViewerId,
+}
+
+impl ViewerIdGenerator {
+    fn next(&mut self) -> ViewerId {
+        let new_id = ViewerId(self.current.0 + 1);
+        mem::replace(&mut self.current, new_id)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Default)]
+struct ViewerId(u32);
 
 pub fn viewer_handler(event_rx: Receiver<Event>, viewer_rx: Receiver<TcpStream>) {
     let viewers = RefCell::new(HashMap::new());
-    let mut current_viewer_idx = 0;
+    let mut viewer_id_generator = ViewerIdGenerator::default();
 
     loop {
         Selector::new()
             .recv(&viewer_rx, |viewer| {
                 info!("received new viewer");
-                handle_new_viewer(&viewers, &mut current_viewer_idx, viewer.unwrap());
+                handle_new_viewer(&viewers, &mut viewer_id_generator, viewer.unwrap());
             })
             .recv(&event_rx, |event| {
                 info!("received event");
@@ -25,28 +40,28 @@ pub fn viewer_handler(event_rx: Receiver<Event>, viewer_rx: Receiver<TcpStream>)
 }
 
 fn handle_new_viewer(
-    viewers: &RefCell<HashMap<i32, TcpStream>>,
-    current_viewer_idx: &mut i32,
+    viewers: &RefCell<HashMap<ViewerId, TcpStream>>,
+    viewer_id_generator: &mut ViewerIdGenerator,
     viewer: TcpStream,
 ) {
-    viewers.borrow_mut().insert(*current_viewer_idx, viewer);
-    *current_viewer_idx += 1;
+    let next_id = viewer_id_generator.next();
+    viewers.borrow_mut().insert(next_id, viewer);
 }
 
-fn handle_new_event(viewers: &RefCell<HashMap<i32, TcpStream>>, event: Event) {
+fn handle_new_event(viewers: &RefCell<HashMap<ViewerId, TcpStream>>, event: Event) {
     let mut closed_viewers = Vec::new();
     send_event_to_viewers(viewers, event, &mut closed_viewers);
     remove_closed_viewers(viewers, closed_viewers.into_iter());
 }
 
 fn send_event_to_viewers(
-    viewers: &RefCell<HashMap<i32, TcpStream>>,
+    viewers: &RefCell<HashMap<ViewerId, TcpStream>>,
     event: Event,
-    closed_viewers: &mut Vec<i32>,
+    closed_viewers: &mut Vec<ViewerId>,
 ) {
     let mut viewers = viewers.borrow_mut();
 
-    for (idx, viewer) in viewers.iter_mut() {
+    for (id, viewer) in viewers.iter_mut() {
         match jsonl::write(viewer, &event) {
             Ok(()) => info!("forwarded event to viewer"),
 
@@ -54,7 +69,7 @@ fn send_event_to_viewers(
                 if io_error.kind() == io::ErrorKind::BrokenPipe =>
             {
                 info!("found closed viewer");
-                closed_viewers.push(*idx);
+                closed_viewers.push(*id);
             }
 
             Err(e) => error!("{:#}", anyhow::Error::new(e)),
@@ -63,15 +78,15 @@ fn send_event_to_viewers(
 }
 
 fn remove_closed_viewers(
-    viewers: &RefCell<HashMap<i32, TcpStream>>,
-    closed_viewers: impl Iterator<Item = i32>,
+    viewers: &RefCell<HashMap<ViewerId, TcpStream>>,
+    closed_viewers: impl Iterator<Item = ViewerId>,
 ) {
     let mut viewers = viewers.borrow_mut();
 
-    for idx in closed_viewers {
+    for id in closed_viewers {
         info!("removed closed viewer");
 
-        let removed_viewer = viewers.remove(&idx);
+        let removed_viewer = viewers.remove(&id);
 
         // we know the viewer we just removed
         // was present in the HashMap

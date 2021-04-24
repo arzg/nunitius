e97adb4d95749450015293dcfa1f11d20ccab014
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-04-24 20:01:21 +1000

    Extract viewer UI to submodule

--- src/viewer.rs
+++ src/viewer.rs
@@ -1,12 +1,13 @@
 mod protocol;
 mod timeline;
+mod ui;
 
 pub use protocol::Protocol;
 pub use timeline::Timeline;
+pub use ui::{render_currently_typing_users, render_event};
 
-use crate::{Color, Event as ServerEvent, EventKind as ServerEventKind, Message, User};
-use chrono::{DateTime, Local, Utc};
-use crossterm::style::{self, style, Styler};
+use crate::{Event as ServerEvent, EventKind as ServerEventKind, Message, User};
+use chrono::{DateTime, Utc};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Event {
@@ -35,57 +36,4 @@ pub enum EventKind {
     Message(Message),
     Login,
     Logout,
-}
-
-pub fn render_event(
-    Event {
-        event,
-        user,
-        time_occurred,
-    }: &Event,
-) -> String {
-    let user = render_user(user);
-
-    let local_time_occurred = time_occurred.with_timezone(&Local);
-    let local_time_occurred = local_time_occurred.format("%H:%M");
-
-    match event {
-        EventKind::Message(Message { body }) => {
-            format!("[{}] {}: {}", local_time_occurred, user, body)
-        }
-        EventKind::Login => format!("[{}] {} logged in!", local_time_occurred, user),
-        EventKind::Logout => format!("[{}] {} logged out!", local_time_occurred, user),
-    }
-}
-
-pub fn render_currently_typing_users<'a>(
-    mut users: impl Iterator<Item = &'a User> + ExactSizeIterator,
-) -> String {
-    let num_users = users.len();
-
-    if num_users == 1 {
-        format!("{} is typing...", render_user(users.next().unwrap()))
-    } else {
-        let users = users.map(render_user).collect::<Vec<_>>().join(" and ");
-        format!("{} are typing...", users)
-    }
-}
-
-fn render_user(user: &User) -> String {
-    let base_styled_content = style(&user.nickname).bold();
-
-    if let Some(ref color) = user.color {
-        let color = match color {
-            Color::Red => style::Color::Red,
-            Color::Green => style::Color::Green,
-            Color::Yellow => style::Color::Yellow,
-            Color::Blue => style::Color::Blue,
-            Color::Magenta => style::Color::Magenta,
-            Color::Cyan => style::Color::Cyan,
-        };
-
-        base_styled_content.with(color).to_string()
-    } else {
-        base_styled_content.to_string()
-    }
 }
---
+++ src/viewer/ui.rs
@@ -0,0 +1,57 @@
+use super::{Event, EventKind};
+use crate::{Color, Message, User};
+use chrono::Local;
+use crossterm::style::{self, style, Styler};
+
+pub fn render_event(
+    Event {
+        event,
+        user,
+        time_occurred,
+    }: &Event,
+) -> String {
+    let user = render_user(user);
+
+    let local_time_occurred = time_occurred.with_timezone(&Local);
+    let local_time_occurred = local_time_occurred.format("%H:%M");
+
+    match event {
+        EventKind::Message(Message { body }) => {
+            format!("[{}] {}: {}", local_time_occurred, user, body)
+        }
+        EventKind::Login => format!("[{}] {} logged in!", local_time_occurred, user),
+        EventKind::Logout => format!("[{}] {} logged out!", local_time_occurred, user),
+    }
+}
+
+pub fn render_currently_typing_users<'a>(
+    mut users: impl Iterator<Item = &'a User> + ExactSizeIterator,
+) -> String {
+    let num_users = users.len();
+
+    if num_users == 1 {
+        format!("{} is typing...", render_user(users.next().unwrap()))
+    } else {
+        let users = users.map(render_user).collect::<Vec<_>>().join(" and ");
+        format!("{} are typing...", users)
+    }
+}
+
+fn render_user(user: &User) -> String {
+    let base_styled_content = style(&user.nickname).bold();
+
+    if let Some(ref color) = user.color {
+        let color = match color {
+            Color::Red => style::Color::Red,
+            Color::Green => style::Color::Green,
+            Color::Yellow => style::Color::Yellow,
+            Color::Blue => style::Color::Blue,
+            Color::Magenta => style::Color::Magenta,
+            Color::Cyan => style::Color::Cyan,
+        };
+
+        base_styled_content.with(color).to_string()
+    } else {
+        base_styled_content.to_string()
+    }
+}

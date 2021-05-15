a12a2bf2887b2692cece92b537b7bff1783e358a
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-05-15 21:25:20 +1000

    Add basic demo of text field to sender

--- rewrite/sender/src/main.rs
+++ rewrite/sender/src/main.rs
@@ -3,9 +3,88 @@ mod text_field;
 
 use crossterm::{cursor, event, queue, terminal};
 use std::convert::TryInto;
+use std::env;
 use std::io::{self, Write};
 
 fn main() -> anyhow::Result<()> {
+    if let Some(subcommand) = env::args().nth(1) {
+        let input = match subcommand.as_str() {
+            "text_field" => run_text_field()?,
+            "editor" => run_editor()?,
+            _ => anyhow::bail!("must specify either ‘text_field’ or ‘editor’ subcommand"),
+        };
+
+        queue!(
+            io::stdout(),
+            terminal::Clear(terminal::ClearType::All),
+            cursor::MoveTo(0, 0)
+        )?;
+
+        println!("You typed: ‘{}’", input);
+    } else {
+        anyhow::bail!("must specify either ‘text_field’ or ‘editor’ subcommand");
+    }
+
+    Ok(())
+}
+
+fn run_text_field() -> anyhow::Result<String> {
+    terminal::enable_raw_mode()?;
+
+    let mut stdout = io::stdout();
+
+    let (terminal_width, _) = terminal::size()?;
+    let mut text_field = text_field::TextField::new((terminal_width - 1).into());
+
+    loop {
+        render_text_field(&text_field, &mut stdout)?;
+
+        let event = event::read()?;
+        match event {
+            event::Event::Key(event::KeyEvent { code, modifiers }) => match (code, modifiers) {
+                (event::KeyCode::Char('c'), event::KeyModifiers::CONTROL)
+                | (event::KeyCode::Enter, _) => break,
+                (event::KeyCode::Char(c), _) => text_field.add(&c.to_string()),
+                (event::KeyCode::Backspace, _) => text_field.backspace(),
+                (event::KeyCode::Up, _) => text_field.move_up(),
+                (event::KeyCode::Down, _) => text_field.move_down(),
+                (event::KeyCode::Left, _) => text_field.move_left(),
+                (event::KeyCode::Right, _) => text_field.move_right(),
+                _ => {}
+            },
+            event::Event::Resize(new_width, _) => text_field.resize((new_width - 1).into()),
+
+            _ => {}
+        }
+    }
+
+    terminal::disable_raw_mode()?;
+
+    Ok(text_field.contents().to_string())
+}
+
+fn render_text_field(
+    text_field: &text_field::TextField,
+    stdout: &mut io::Stdout,
+) -> anyhow::Result<()> {
+    queue!(
+        stdout,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::MoveTo(0, 0)
+    )?;
+
+    write!(stdout, "{}", text_field.render())?;
+
+    let column = text_field.cursor();
+    let column = column.try_into().unwrap();
+    queue!(stdout, cursor::MoveTo(column, 0))?;
+
+    stdout.flush()?;
+
+    Ok(())
+}
+
+fn run_editor() -> anyhow::Result<String> {
     terminal::enable_raw_mode()?;
 
     let mut stdout = io::stdout();
@@ -14,7 +93,7 @@ fn main() -> anyhow::Result<()> {
     let mut editor = editor::Editor::new((terminal_width - 1).into(), terminal_height.into());
 
     loop {
-        render(&editor, &mut stdout)?;
+        render_editor(&editor, &mut stdout)?;
 
         let event = event::read()?;
         match event {
@@ -39,10 +118,10 @@ fn main() -> anyhow::Result<()> {
 
     terminal::disable_raw_mode()?;
 
-    Ok(())
+    Ok(editor.contents())
 }
 
-fn render(editor: &editor::Editor, stdout: &mut io::Stdout) -> anyhow::Result<()> {
+fn render_editor(editor: &editor::Editor, stdout: &mut io::Stdout) -> anyhow::Result<()> {
     queue!(
         stdout,
         terminal::Clear(terminal::ClearType::All),
